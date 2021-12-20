f634f4e1624c2975b2a6df13573a6fdf258f8ca2
HundredCleanWater <maxmp9904@gmail.com>
2021-12-20 13:54:53 +0900

    1

---
+++ src/main/java/com/example/nono/nono.java
@@ -0,0 +1,4 @@
+package com.example.nono;
+
+public class nono {
+}
---
+++ src/test/java/com/example/nono/NonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
